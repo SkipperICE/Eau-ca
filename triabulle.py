fe48208a62bae36fa5de7b0586de1c2af621d431
SkipperICE <121302022+SkipperICE@users.noreply.github.com>
2023-03-29 17:30:31 +0200

    tri a bulle

---
+++ triabulle.py
@@ -0,0 +1,30 @@
+import sys
+
+# Définition de la fonction de tri à bulle
+def my_bubble_sort(array):
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+    return array
+
+# Vérification des arguments
+if len(sys.argv) < 2:
+    print("error")
+    sys.exit()
+
+# Conversion des arguments en nombres
+try:
+    nums = [int(arg) for arg in sys.argv[1:]]
+except ValueError:
+    print("Donne des nombres chackal")
+    sys.exit()
+
+# Tri de la liste
+sorted_nums = my_bubble_sort(nums)
+print(sorted_nums)
+# Affichage de la liste triée
+for num in sorted_nums:
+    print(num, end=' ')
+print()
